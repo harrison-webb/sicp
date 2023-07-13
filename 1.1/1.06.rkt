64c5a07ad1f4210b608ee87c74294194f3a3c585
Harrison Webb <harrison.dev.00@gmail.com>
2023-07-13 01:01:41 -0600

    section 1.1 exercise 1.6

--- 1.1/1.06.rkt
+++ 1.1/1.06.rkt
@@ -16,8 +16,11 @@
 (define (average x y)
   (/ (+ x y) 2))
 
+(define (square x)
+  (* x x))
+
 (define (good-enough? guess x)
-  (< (abs (- square guess) x) 0.001))
+  (< (abs (- (square guess) x)) 0.001))
 
 (define (improve guess x)
   (average guess (/ x guess)))
@@ -28,13 +31,13 @@
     (sqrt-iter (improve guess x) x)))
 
 (define (sqrt x)
-  (squr-iter 1.0 x))
+  (sqrt-iter 1.0 x))
 
 
 ; new square-root program
 
 (define (test x)
-  (+ (1 x)))
+  (+ 1 x))
 
 (define (new-sqrt-iter guess x)
   (new-if (good-enough? guess x)
@@ -44,7 +47,12 @@
 (define (new-sqrt x)
   (new-sqrt-iter 1.0 x))
 
-(new-sqrt 1)
+; (new-sqrt 1)
 (good-enough? 1 2)
 
-(test (1))
+(test 1)
+
+
+; Due to lisp's applicative-order evaluation where a procedure's arguments first get evaluated and then the procedure body executes, the 
+; square root program using Alyssa's new-if will enter an infinite loop. This is because sqrt-iter is recursively called in the else clause, and since
+; the new-if does not immediately return if true, the else-clause argument will be infinitely evaluated.
